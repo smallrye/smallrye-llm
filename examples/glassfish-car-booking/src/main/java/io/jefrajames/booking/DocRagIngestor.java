b77fa8d98e47143799f1cb4acdb5f42c102ce7a6
Yann Blazart <yann.blazart@gmail.com>
2024-08-21 17:55:57 +0200

    Fix use of 0.33 and changes in packages for dev.langchain4j.model.embedding.onnx.allminilml6v2

--- examples/glassfish-car-booking/src/main/java/io/jefrajames/booking/DocRagIngestor.java
+++ examples/glassfish-car-booking/src/main/java/io/jefrajames/booking/DocRagIngestor.java
@@ -13,7 +13,7 @@ import jakarta.enterprise.inject.Produces;
 import dev.langchain4j.data.document.Document;
 import dev.langchain4j.data.document.parser.TextDocumentParser;
 import dev.langchain4j.data.document.splitter.DocumentSplitters;
-import dev.langchain4j.model.embedding.AllMiniLmL6V2EmbeddingModel;
+import dev.langchain4j.model.embedding.onnx.allminilml6v2.AllMiniLmL6V2EmbeddingModel;
 import dev.langchain4j.model.embedding.EmbeddingModel;
 import dev.langchain4j.store.embedding.EmbeddingStoreIngestor;
 import dev.langchain4j.store.embedding.inmemory.InMemoryEmbeddingStore;
