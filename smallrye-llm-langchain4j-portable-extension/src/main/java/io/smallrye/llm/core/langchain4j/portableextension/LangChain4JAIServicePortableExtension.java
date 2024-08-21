f898a3cca890367307e631830b19d7a5221a5c73
Buhake Sindi <buhake.sindi@sindi.co.za>
2024-08-21 12:56:57 +0200

    Added features and bug fixes from experience working with extensions.

--- smallrye-llm-langchain4j-portable-extension/src/main/java/io/smallrye/llm/core/langchain4j/portableextension/LangChain4JAIServicePortableExtension.java
+++ smallrye-llm-langchain4j-portable-extension/src/main/java/io/smallrye/llm/core/langchain4j/portableextension/LangChain4JAIServicePortableExtension.java
@@ -4,11 +4,13 @@ import java.util.HashSet;
 import java.util.Set;
 
 import jakarta.enterprise.event.Observes;
+import jakarta.enterprise.inject.Instance;
 import jakarta.enterprise.inject.spi.AfterBeanDiscovery;
 import jakarta.enterprise.inject.spi.BeanManager;
 import jakarta.enterprise.inject.spi.CDI;
 import jakarta.enterprise.inject.spi.Extension;
 import jakarta.enterprise.inject.spi.ProcessAnnotatedType;
+import jakarta.enterprise.inject.spi.ProcessInjectionPoint;
 import jakarta.enterprise.inject.spi.WithAnnotations;
 
 import org.jboss.logging.Logger;
@@ -35,6 +37,21 @@ public class LangChain4JAIServicePortableExtension implements Extension {
         }
     }
 
+    /**
+     * This is useful for application servers that can't support proccessAnnotatedType.
+     *
+     * @param event
+     */
+    void processInjectionPoints(@Observes ProcessInjectionPoint<?, ?> event) {
+        if (event.getInjectionPoint().getBean() == null) {
+            processInjectionPoint(event);
+        }
+
+        if (Instance.class.equals(Reflections.getRawType(event.getInjectionPoint().getType()))) {
+            processInjectionPoint(event);
+        }
+    }
+
     void afterBeanDiscovery(@Observes AfterBeanDiscovery afterBeanDiscovery, BeanManager beanManager)
             throws ClassNotFoundException {
         for (Class<?> aiServiceClass : detectedAIServicesDeclaredInterfaces) {
@@ -43,7 +60,21 @@ public class LangChain4JAIServicePortableExtension implements Extension {
             afterBeanDiscovery.addBean()
                     .types(aiServiceClass)
                     .scope(annotation.scope())
+                    .name("registeredAIService-" + aiServiceClass.getName()) //Without this, the container won't create a CreationalContext
                     .createWith(creationalContext -> CommonAIServiceCreator.create(CDI.current(), aiServiceClass));
+        }
+    }
+
+    private void processInjectionPoint(ProcessInjectionPoint<?, ?> event) {
+        Class<?> rawType = Reflections.getRawType(event.getInjectionPoint().getType());
+        if (rawType.isInterface()) {
+            RegisterAIService annotation = rawType.getAnnotation(RegisterAIService.class);
+            if (annotation == null) {
+                event.addDefinitionError(new RuntimeException("The class '" + rawType.getName()
+                        + "'is annotated with @RegisterAIService, but only interface(s) are allowed."));
+            } else {
+                detectedAIServicesDeclaredInterfaces.add(rawType);
+            }
         }
     }
 }
