8648300fa4b84f9120dfbfde854c1c1adf4e8871
Buhake Sindi <buhake.sindi@sindi.co.za>
2024-08-31 02:12:55 +0200

    Updated Portable extension that supports Instance<X> injection.

--- smallrye-llm-langchain4j-portable-extension/src/main/java/io/smallrye/llm/core/langchain4j/portableextension/LangChain4JAIServicePortableExtension.java
+++ smallrye-llm-langchain4j-portable-extension/src/main/java/io/smallrye/llm/core/langchain4j/portableextension/LangChain4JAIServicePortableExtension.java
@@ -3,10 +3,6 @@ package io.smallrye.llm.core.langchain4j.portableextension;
 import java.util.HashSet;
 import java.util.Set;
 
-import org.jboss.logging.Logger;
-
-import io.smallrye.llm.aiservice.CommonAIServiceCreator;
-import io.smallrye.llm.spi.RegisterAIService;
 import jakarta.enterprise.event.Observes;
 import jakarta.enterprise.inject.Instance;
 import jakarta.enterprise.inject.spi.AfterBeanDiscovery;
@@ -20,15 +16,21 @@ import jakarta.enterprise.inject.spi.ProcessInjectionPoint;
 import jakarta.enterprise.inject.spi.WithAnnotations;
 import jakarta.enterprise.inject.spi.configurator.BeanConfigurator;
 
+import org.jboss.logging.Logger;
+
+import io.smallrye.llm.aiservice.CommonAIServiceCreator;
+import io.smallrye.llm.spi.RegisterAIService;
+
 public class LangChain4JAIServicePortableExtension implements Extension {
     private static final Logger LOGGER = Logger.getLogger(LangChain4JAIServicePortableExtension.class);
+    private static final Set<Class<?>> detectedAIServicesDeclaredInterfaces = new HashSet<>();
     private Set<AnnotatedType<?>> annotatedTypes = new HashSet<>();
     private Set<InjectionPoint> componentInjectionPoints = new HashSet<>();
     private Set<InjectionPoint> instanceInjectionPoints = new HashSet<>();
 
-//    public static Set<Class<?>> getDetectedAIServicesDeclaredInterfaces() {
-//        return detectedAIServicesDeclaredInterfaces;
-//    }
+    public static Set<Class<?>> getDetectedAIServicesDeclaredInterfaces() {
+        return detectedAIServicesDeclaredInterfaces;
+    }
 
     <T> void processAnnotatedType(@Observes @WithAnnotations({ RegisterAIService.class }) ProcessAnnotatedType<T> pat) {
         if (pat.getAnnotatedType().getJavaClass().isInterface()) {
@@ -48,51 +50,54 @@ public class LangChain4JAIServicePortableExtension implements Extension {
      */
     void processInjectionPoints(@Observes ProcessInjectionPoint<?, ?> event) {
         if (event.getInjectionPoint().getBean() == null) {
-        	componentInjectionPoints.add(event.getInjectionPoint());
+            componentInjectionPoints.add(event.getInjectionPoint());
         }
 
         if (Instance.class.equals(Reflections.getRawType(event.getInjectionPoint().getType()))) {
-        	instanceInjectionPoints.add(event.getInjectionPoint());
+            instanceInjectionPoints.add(event.getInjectionPoint());
         }
     }
 
     void afterBeanDiscovery(@Observes AfterBeanDiscovery afterBeanDiscovery, BeanManager beanManager)
             throws ClassNotFoundException {
-    	for (AnnotatedType<?> annotatedType : annotatedTypes) {
-			LOGGER.info("Adding AiService of interface '" + annotatedType.getJavaClass().getName()  + "', discovered during processAnnotatedType(), for component injection.");
-			RegisterAIService aiServiceAnnotation = annotatedType.getJavaClass().getAnnotation(RegisterAIService.class);
-			addBean(afterBeanDiscovery, annotatedType.getJavaClass(), aiServiceAnnotation, false);
-		}
-		
-		for (InjectionPoint ip : componentInjectionPoints) {
-			Class<?> rawType = Reflections.getRawType(ip.getType());
-			RegisterAIService aiServiceAnnotation = rawType.getAnnotation(RegisterAIService.class);
-			addBean(afterBeanDiscovery, rawType, aiServiceAnnotation, false);
-		}
-		
-		for (InjectionPoint ip : instanceInjectionPoints) {
-			Class<?> rawType = Reflections.getRawType(ip.getType());
-			RegisterAIService aiServiceAnnotation = rawType.getAnnotation(RegisterAIService.class);
-			addBean(afterBeanDiscovery, rawType, aiServiceAnnotation, true);
-		}
-    }
-    
+        for (AnnotatedType<?> annotatedType : annotatedTypes) {
+            LOGGER.info("Adding RegisterAIService of interface '" + annotatedType.getJavaClass().getName()
+                    + "', discovered during processAnnotatedType(), for component injection.");
+            RegisterAIService registerAiServiceAnnotation = annotatedType.getJavaClass().getAnnotation(RegisterAIService.class);
+            addBean(afterBeanDiscovery, annotatedType.getJavaClass(), registerAiServiceAnnotation, false);
+        }
 
-	
-	private void addBean(AfterBeanDiscovery abd, Class<?> interfaceClass, RegisterAIService aiServiceAnnotation, boolean produce) {
-		if (!interfaceClass.isInterface() || aiServiceAnnotation == null) return ;
-		
-		BeanConfigurator<Object> bc = abd.addBean()
-				.scope(aiServiceAnnotation.scope())
-				.types(interfaceClass)
-				.name("registeredAIService-" + interfaceClass.getName()); //Without this, the container won't create a CreationalContext
-		
-		if (produce) {
-			bc.produceWith(c -> CommonAIServiceCreator.create(CDI.current(), interfaceClass));
-		} else {
-			bc.createWith(c -> CommonAIServiceCreator.create(CDI.current(), interfaceClass));
-		}
-		
-		LOGGER.info("Added AiService of interface type '" + interfaceClass.getName()  + "' for " + (produce ? "instance" : "component")  + " injection.");
-	}
+        for (InjectionPoint ip : componentInjectionPoints) {
+            Class<?> rawType = Reflections.getRawType(ip.getType());
+            RegisterAIService registerAiServiceAnnotation = rawType.getAnnotation(RegisterAIService.class);
+            addBean(afterBeanDiscovery, rawType, registerAiServiceAnnotation, false);
+        }
+
+        for (InjectionPoint ip : instanceInjectionPoints) {
+            Class<?> rawType = Reflections.getRawType(ip.getType());
+            RegisterAIService registerAiServiceAnnotation = rawType.getAnnotation(RegisterAIService.class);
+            addBean(afterBeanDiscovery, rawType, registerAiServiceAnnotation, true);
+        }
+    }
+
+    private void addBean(AfterBeanDiscovery abd, Class<?> interfaceClass, RegisterAIService registerAiServiceAnnotation,
+            boolean produce) {
+        if (!interfaceClass.isInterface() || registerAiServiceAnnotation == null)
+            return;
+
+        detectedAIServicesDeclaredInterfaces.add(interfaceClass);
+        BeanConfigurator<Object> bc = abd.addBean()
+                .scope(registerAiServiceAnnotation.scope())
+                .types(interfaceClass)
+                .name("registeredAIService-" + interfaceClass.getName()); //Without this, the container won't create a CreationalContext
+
+        if (produce) {
+            bc.produceWith(c -> CommonAIServiceCreator.create(CDI.current(), interfaceClass));
+        } else {
+            bc.createWith(c -> CommonAIServiceCreator.create(CDI.current(), interfaceClass));
+        }
+
+        LOGGER.info("Added RegisterAIService of interface type '" + interfaceClass.getName() + "' for "
+                + (produce ? "instance" : "component") + " injection.");
+    }
 }
