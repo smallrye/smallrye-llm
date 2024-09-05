70117106dc8495652612792d843ac578b514d67d
Buhake Sindi <buhake_sindi@hotmail.com>
2024-09-05 16:40:34 +0200

    Adding BOM, ESA & integration

---
+++ smallrye-llm-langchain4j-liberty-bundle/integration/src/main/java/io/smallrye/llm/core/langchain4j/spi/liberty/cdi/internal/Langchain4JCDIExtensionMetadata.java
@@ -0,0 +1,43 @@
+package io.smallrye.llm.core.langchain4j.spi.liberty.cdi.internal;
+
+import java.lang.annotation.Annotation;
+import java.util.Set;
+
+import jakarta.enterprise.inject.spi.Extension;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.ConfigurationPolicy;
+
+import io.openliberty.cdi.spi.CDIExtensionMetadata;
+import io.smallrye.llm.core.langchain4j.portableextension;
+import io.smallrye.llm.spi;
+
+/**
+ * @author Buhake Sindi
+ * @since 26 August 2024
+ */
+@Component(service = CDIExtensionMetadata.class, configurationPolicy = ConfigurationPolicy.IGNORE, immediate = true)
+public class Langchain4JCDIExtensionMetadata implements CDIExtensionMetadata {
+
+    /*
+     * (non-Javadoc)
+     *
+     * @see io.openliberty.cdi.spi.CDIExtensionMetadata#getBeanDefiningAnnotationClasses()
+     */
+    @Override
+    public Set<Class<? extends Annotation>> getBeanDefiningAnnotationClasses() {
+        // TODO Auto-generated method stub
+        return Set.of(RegisterAIService.class);
+    }
+
+    /*
+     * (non-Javadoc)
+     *
+     * @see io.openliberty.cdi.spi.CDIExtensionMetadata#getExtensions()
+     */
+    @Override
+    public Set<Class<? extends Extension>> getExtensions() {
+        // TODO Auto-generated method stub
+        return Set.of(LangChain4JAIServicePortableExtension.class);
+    }
+}
