8967e17f0430a37d036a717d1070013f752f3a9c
Buhake Sindi <buhake.sindi@sindi.co.za>
2024-09-16 13:56:19 +0200

    Adding Liberty examples.

--- smallrye-llm-langchain4j-liberty-bundle/integration/src/main/java/io/smallrye/llm/core/langchain4j/spi/liberty/cdi/internal/Langchain4JCDIExtensionMetadata.java
+++ smallrye-llm-langchain4j-liberty-bundle/integration/src/main/java/io/smallrye/llm/core/langchain4j/spi/liberty/cdi/internal/Langchain4JCDIExtensionMetadata.java
@@ -3,6 +3,8 @@ package io.smallrye.llm.core.langchain4j.spi.liberty.cdi.internal;
 import java.lang.annotation.Annotation;
 import java.util.Set;
 
+import jakarta.enterprise.inject.spi.Extension;
+
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.ConfigurationPolicy;
 
@@ -10,7 +12,6 @@ import io.openliberty.cdi.spi.CDIExtensionMetadata;
 import io.smallrye.llm.core.langchain4j.portableextension.LangChain4JAIServicePortableExtension;
 import io.smallrye.llm.core.langchain4j.portableextension.LangChain4JPluginsPortableExtension;
 import io.smallrye.llm.spi.RegisterAIService;
-import jakarta.enterprise.inject.spi.Extension;
 
 /**
  * @author Buhake Sindi
@@ -19,21 +20,25 @@ import jakarta.enterprise.inject.spi.Extension;
 @Component(service = CDIExtensionMetadata.class, configurationPolicy = ConfigurationPolicy.IGNORE, immediate = true)
 public class Langchain4JCDIExtensionMetadata implements CDIExtensionMetadata {
 
-	/* (non-Javadoc)
-	 * @see io.openliberty.cdi.spi.CDIExtensionMetadata#getBeanDefiningAnnotationClasses()
-	 */
-	@Override
-	public Set<Class<? extends Annotation>> getBeanDefiningAnnotationClasses() {
-		// TODO Auto-generated method stub
-		return Set.of(RegisterAIService.class);
-	}
+    /*
+     * (non-Javadoc)
+     *
+     * @see io.openliberty.cdi.spi.CDIExtensionMetadata#getBeanDefiningAnnotationClasses()
+     */
+    @Override
+    public Set<Class<? extends Annotation>> getBeanDefiningAnnotationClasses() {
+        // TODO Auto-generated method stub
+        return Set.of(RegisterAIService.class);
+    }
 
-	/* (non-Javadoc)
-	 * @see io.openliberty.cdi.spi.CDIExtensionMetadata#getExtensions()
-	 */
-	@Override
-	public Set<Class<? extends Extension>> getExtensions() {
-		// TODO Auto-generated method stub
-		return Set.of(LangChain4JAIServicePortableExtension.class, LangChain4JPluginsPortableExtension.class);
-	}
+    /*
+     * (non-Javadoc)
+     *
+     * @see io.openliberty.cdi.spi.CDIExtensionMetadata#getExtensions()
+     */
+    @Override
+    public Set<Class<? extends Extension>> getExtensions() {
+        // TODO Auto-generated method stub
+        return Set.of(LangChain4JAIServicePortableExtension.class, LangChain4JPluginsPortableExtension.class);
+    }
 }
