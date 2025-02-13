adae42d0ef2b541f1a50d52f5b5d7ed71377fa49
Buhake Sindi <TheEliteGentleman@users.noreply.github.com>
2025-02-13 21:01:01 +0200

    Moved Fault Tolerance to its own module. (#92)
    * Moved Fault Tolerance to its own module.
    
    * Trying to clean things
    Signed-off-by: Emmanuel Hugonnet <ehugonne@redhat.com>
    
    Signed-off-by: Emmanuel Hugonnet <ehugonne@redhat.com>
    
    ---------
    
    Signed-off-by: Emmanuel Hugonnet <ehugonne@redhat.com>
    Co-authored-by: Emmanuel Hugonnet <ehugonne@redhat.com>

--- smallrye-llm-langchain4j-fault-tolerance/src/main/java/io/smallrye/llm/langchain4j/faulttolerance/spi/Langchain4JFaultToleranceExtension.java
+++ smallrye-llm-langchain4j-fault-tolerance/src/main/java/io/smallrye/llm/langchain4j/faulttolerance/spi/Langchain4JFaultToleranceExtension.java
@@ -1,10 +1,11 @@
-package io.smallrye.llm.core.langchain4j.portableextension;
+package io.smallrye.llm.langchain4j.faulttolerance.spi;
 
 import java.util.logging.Logger;
 
 import jakarta.enterprise.event.Observes;
 import jakarta.enterprise.inject.spi.AnnotatedMethod;
 import jakarta.enterprise.inject.spi.AnnotatedType;
+import jakarta.enterprise.inject.spi.Bean;
 import jakarta.enterprise.inject.spi.BeanManager;
 import jakarta.enterprise.inject.spi.DeploymentException;
 import jakarta.enterprise.inject.spi.Extension;
@@ -12,9 +13,12 @@ import jakarta.enterprise.inject.spi.ProcessSyntheticBean;
 
 import org.eclipse.microprofile.faulttolerance.exceptions.FaultToleranceDefinitionException;
 
+import io.smallrye.faulttolerance.FaultToleranceBinding;
 import io.smallrye.faulttolerance.autoconfig.FaultToleranceMethod;
 import io.smallrye.faulttolerance.config.FaultToleranceMethods;
 import io.smallrye.faulttolerance.config.FaultToleranceOperation;
+import io.smallrye.llm.core.langchain4j.portableextension.LangChain4JAIServiceBean;
+import io.smallrye.llm.spi.RegisterAIService;
 
 /**
  * @author Buhake Sindi
@@ -24,16 +28,23 @@ public class Langchain4JFaultToleranceExtension implements Extension {
 
     private static final Logger LOGGER = Logger.getLogger(Langchain4JFaultToleranceExtension.class.getName());
 
-    void validateFaultToleranceOperations(@Observes ProcessSyntheticBean<?> event, BeanManager bm) {
-        LOGGER.info("validateFaultToleranceOperations: Synthetic Event -> " + event.getBean().getBeanClass());
+    <X> void validateFaultToleranceOperations(@Observes ProcessSyntheticBean<X> event, BeanManager bm) {
+        Bean<X> bean = event.getBean();
+        LOGGER.info("validateFaultToleranceOperations: Synthetic Event -> " + bean.getBeanClass());
         try {
-            AnnotatedType<?> annotatedType = bm.createAnnotatedType(event.getBean().getBeanClass());
+            AnnotatedType<?> annotatedType = bm.createAnnotatedType(bean.getBeanClass());
             for (AnnotatedMethod<?> annotatedMethod : annotatedType.getMethods()) {
                 FaultToleranceMethod method = FaultToleranceMethods.create(annotatedType.getJavaClass(), annotatedMethod);
                 if (method.isLegitimate()) {
                     FaultToleranceOperation operation = FaultToleranceOperation.create(method);
                     operation.validate();
                     LOGGER.info("Found: " + operation);
+
+                    if (bean.getStereotypes().contains(RegisterAIService.class)) {
+                        //Add Fault Tolerance interceptor Binding
+                        ((LangChain4JAIServiceBean<X>) bean).getInterceptorBindings()
+                                .add(FaultToleranceBinding.Literal.INSTANCE);
+                    }
                 }
             }
         } catch (FaultToleranceDefinitionException e) {
