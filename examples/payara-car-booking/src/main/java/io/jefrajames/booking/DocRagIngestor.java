11513b8d022e70ce4c5e15175a7d9597a6785177
Buhake Sindi <buhake_sindi@hotmail.com>
2025-04-10 02:28:30 +0200

    Adding Payara example.

--- examples/payara-car-booking/src/main/java/io/jefrajames/booking/DocRagIngestor.java
+++ examples/payara-car-booking/src/main/java/io/jefrajames/booking/DocRagIngestor.java
@@ -10,6 +10,8 @@ import jakarta.enterprise.context.Initialized;
 import jakarta.enterprise.event.Observes;
 import jakarta.enterprise.inject.Produces;
 
+import org.jboss.logging.Logger;
+
 import dev.langchain4j.data.document.Document;
 import dev.langchain4j.data.document.parser.TextDocumentParser;
 import dev.langchain4j.data.document.splitter.DocumentSplitters;
@@ -17,11 +19,11 @@ import dev.langchain4j.model.embedding.onnx.allminilml6v2.AllMiniLmL6V2Embedding
 import dev.langchain4j.model.embedding.EmbeddingModel;
 import dev.langchain4j.store.embedding.EmbeddingStoreIngestor;
 import dev.langchain4j.store.embedding.inmemory.InMemoryEmbeddingStore;
-import lombok.extern.java.Log;
 
-@Log
 @ApplicationScoped
 public class DocRagIngestor {
+	
+	private static final Logger LOGGER = Logger.getLogger(DocRagIngestor.class.getName());
 
     // Used by ContentRetriever
     @Produces
@@ -50,7 +52,7 @@ public class DocRagIngestor {
         List<Document> docs = loadDocs();
         ingestor.ingest(docs);
 
-        log.info(String.format("DEMO %d documents ingested in %d msec", docs.size(),
+        LOGGER.info(String.format("DEMO %d documents ingested in %d msec", docs.size(),
                 System.currentTimeMillis() - start));
     }
 
